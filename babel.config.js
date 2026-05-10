eab052f38238f778e177bb4ec781264e76ccc088
ZangEdward <75204945+ZangEdward@users.noreply.github.com>
2026-05-11 02:13:17 +0800

    更新 babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo'], // SDK 51 只需要这一个 preset 即可自动处理 router
     plugins: [
-      // 如果你使用了 expo-router，通常需要这个
-      'expo-router/babel',
-      // 如果代码里用了装饰器（常见于某些 TV 端的库）
-      ['@babel/plugin-proposal-decorators', { legacy: true }],
-      'react-native-reanimated/plugin', // 如果用到了动画库，必须放在最后
+      // 注意：如果你使用了 react-native-reanimated，请务必保留下面这一行
+      // 且它必须放在插件列表的最后面
+      'react-native-reanimated/plugin',
     ],
   };
 };
