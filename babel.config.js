6b98094af2f5f48b27269e4881ff127f1b4551a4
ZangEdward <75204945+ZangEdward@users.noreply.github.com>
2026-05-11 00:42:25 +0800

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,13 @@
 module.exports = function (api) {
   api.cache(true);
-  
-  const plugins = [];
-  
-  // 在生产环境移除console调用以优化性能
-  if (process.env.NODE_ENV === 'production') {
-    plugins.push('transform-remove-console');
-  }
-  
   return {
     presets: ['babel-preset-expo'],
-    plugins,
+    plugins: [
+      // 如果你使用了 expo-router，通常需要这个
+      'expo-router/babel',
+      // 如果代码里用了装饰器（常见于某些 TV 端的库）
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      'react-native-reanimated/plugin', // 如果用到了动画库，必须放在最后
+    ],
   };
 };
