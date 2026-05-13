a35dea546318069082abad933816e990cff9e70d
ZangEdward <75204945+ZangEdward@users.noreply.github.com>
2026-05-13 23:21:49 +0800

    Create apiNodes.ts

---
+++ services/apiNodes.ts
@@ -0,0 +1,6 @@
+export const API_NODES = [
+  { key: "ltv", label: "默认节点（ltv.955598.xyz）", url: "https://ltv.955598.xyz" },
+  { key: "atv", label: "亚马逊节点（atv.955598.xyz）", url: "https://atv.955598.xyz" },
+  { key: "ctv", label: "edge节点（ctv.955598.xyz）", url: "https://ctv.955598.xyz" },
+  { key: "edu", label: "教育节点（ltv.lzsb.edu.eu.org）", url: "https://ltv.lzsb.edu.eu.org" },
+];
