cc64788b969ffc7b4a0b40f543503b6cc3d5ae4a
Your Name <you@example.com>
2026-06-11 11:26:36 +0800

    修复编译2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
     }
 
     defaultConfig {
