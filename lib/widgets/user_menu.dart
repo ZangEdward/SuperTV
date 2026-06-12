99832672d9373f5d9f74558b3f7ff62751f49f80
Your Name <you@example.com>
2026-06-12 11:27:38 +0800

    云盘搜索

--- lib/widgets/user_menu.dart
+++ lib/widgets/user_menu.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:package_info_plus/package_info_plus.dart';
